efd48ac82825e8dd4f34b38b9172e70b6160ce05
DGC2129 <104290421+DGC2129@users.noreply.github.com>
2025-02-20 12:27:39 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,26 +1,26 @@
 
 terraform {
-  required_version = ">= 1.10.5" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "<= 1.10.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6.3"
+      version = "<= 3.6.3"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.35.1"
+      version = "<=2.35.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.87.0"
+      ersion = "<= 6.0.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.5.2"
+      version = "<= 2.5.2"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.6"
+      version = "<= 2.3.6"
     }
   }
 }
